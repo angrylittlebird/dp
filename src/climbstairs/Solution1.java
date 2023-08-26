679f06f35b4c8e7d45b9ac6b02ccd65c42155d46
imp <1339446165@qq.com>
2023-08-26 17:13:19 +0800

    dfs

---
+++ src/climbstairs/Solution1.java
@@ -0,0 +1,18 @@
+package climbstairs;
+
+public class Solution1 {
+    public int climbStairs(int n) {
+        Integer[] memo = new Integer[n + 1];
+        return dfs(n, memo);
+    }
+
+    //f(n) = f(n-1) + f(n-2)
+    //走到第N个台阶有几种方式
+    private int dfs(int n, Integer[] memo) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if(memo[n] != null) return memo[n];
+
+        return memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo);
+    }
+}
