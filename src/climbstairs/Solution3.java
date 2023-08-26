14e70b30ce9d554e13af95af3016c82c636b0399
imp <1339446165@qq.com>
2023-08-26 17:15:37 +0800

    status compress

---
+++ src/climbstairs/Solution3.java
@@ -0,0 +1,30 @@
+package climbstairs;
+
+public class Solution3 {
+    public int climbStairs(int n) {
+        if (n < 3) return n;
+
+        //init
+        int a = 1;
+        int b = 2;
+
+        int cur = 0;
+        for (int i = 3; i <= n; i++) {
+            cur = a + b;
+
+            a = b;
+            b = cur;
+        }
+        return cur;
+    }
+
+    //f(n) = f(n-1) + f(n-2)
+    //走到第N个台阶有几种方式
+    private int dfs(int n, Integer[] memo) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if(memo[n] != null) return memo[n];
+
+        return memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo);
+    }
+}
