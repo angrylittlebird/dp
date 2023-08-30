76c2bbaeeffad86bad7cfe6409b3b3aa9e1eb038
imp <1339446165@qq.com>
2023-08-30 10:55:36 +0800

    coinChange with memory

--- src/coinchange/Solution1.java
+++ src/coinchange/Solution1.java
@@ -1,5 +1,18 @@
 package coinchange;
 
+//1. 这个问题的base case是什么？
+// 总金额为0时，所需的硬币个数为0
+//2. 这个问题有什么“状态”？
+// 目标金额amount
+//3. 对于每个“状态”，可以做出什么“选择”使得“状态”发生改变？
+// coin = {1, 5, 11}, amount = 15
+//f(15): 凑成面额为15的最少硬币数
+//        f(15) = f(15 - 1) + 1;
+//        f(15) = f(15 - 5) + 1;
+//        f(15) = f(15 - 11) + 1;
+//        f(15) = min{ f(14), f(10), f(4) } + 1;
+//4. 如何定义 dp数组/函数 的含义来表现“状态”和“选择”？
+//        f(i) = min{ f(i-coin) } + 1, coin ∈ coins; i >= coin
 public class Solution1 {
     int INF = Integer.MAX_VALUE / 2;
 
@@ -24,7 +37,6 @@ public class Solution1 {
 
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
-//        solution1.coinChange(new int[]{1, 5, 11}, 12);
-        solution1.coinChange(new int[]{5, 11}, 6);
+        solution1.coinChange(new int[]{1, 5, 11}, 12);
     }
 }
